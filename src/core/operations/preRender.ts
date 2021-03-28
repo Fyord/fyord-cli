7b742111885367bd682650ac64a9baf63ecc2202
Joey Bayes <joey.bayes@uscca.com>
2021-03-28 13:23:07 -0400

    Refactor pre render to be more testable.

--- src/core/operations/preRender.ts
+++ src/core/operations/preRender.ts
@@ -1,9 +1,8 @@
-/* eslint-disable no-console */
-import * as fse from 'fs-extra';
 import * as puppeteer from 'puppeteer';
+import { FileSystemExtraAdapter, IFileSystemExtraAdapter } from '../../fileSystem/module';
 import { Command, Result, Strings } from 'tsbase';
 import { Settings } from '../../settings/settings';
-import { SettingsService } from '../../settings/settingsService';
+import { ISettingsService, SettingsService } from '../../settings/settingsService';
 import { IOperation } from './operation';
 
 export interface IPuppeteer {
@@ -11,110 +10,50 @@ export interface IPuppeteer {
 }
 
 export class PreRenderOperation implements IOperation {
+  private config: {
+    baseUrl: string | string[],
+    outputPathRoot: string | string[],
+    blockedResourceTypes: string[],
+    skippedResources: string[],
+    dynamicRenderModeString: string | string[],
+    staticRenderModeString: string | string[],
+    hybridRenderModeString: string | string[],
+    bundleScriptRegex: RegExp,
+    unsupportedBrowserScript: string | string[],
+  };
+
+  private pagesToCrawl = [Strings.Empty];
+  private crawledPages = ['/'];
+  private errors = new Array();
+  private siteMap = new Array();
+
+  constructor(
+    private browser: IPuppeteer = puppeteer,
+    private fse: IFileSystemExtraAdapter = FileSystemExtraAdapter,
+    settingsService: ISettingsService = SettingsService.Instance()
+  ) {
+    this.config = {
+      baseUrl: settingsService.GetSettingOrDefault(Settings.BaseUrl),
+      outputPathRoot: settingsService.GetSettingOrDefault(Settings.OutputPathRoot),
+      blockedResourceTypes: settingsService.GetSettingOrDefault(Settings.BlockedResourceTypes) as string[],
+      skippedResources: settingsService.GetSettingOrDefault(Settings.SkippedResources) as string[],
+      dynamicRenderModeString: settingsService.GetSettingOrDefault(Settings.DynamicRenderModeString),
+      staticRenderModeString: settingsService.GetSettingOrDefault(Settings.StaticRenderModeString),
+      hybridRenderModeString: settingsService.GetSettingOrDefault(Settings.HybridRenderModeString),
+      bundleScriptRegex: new RegExp(settingsService.GetSettingOrDefault(Settings.BundleScriptRegex) as string),
+      unsupportedBrowserScript: settingsService.GetSettingOrDefault(Settings.UnsupportedBrowserScript)
+    };
+  }
+
   public Execute(): Result {
     return new Command(() => {
-      const settingsService = SettingsService.Instance();
-
-      const config = {
-        baseUrl: settingsService.GetSettingOrDefault(Settings.BaseUrl),
-        outputPathRoot: settingsService.GetSettingOrDefault(Settings.OutputPathRoot),
-        blockedResourceTypes: settingsService.GetSettingOrDefault(Settings.BlockedResourceTypes) as string[],
-        skippedResources: settingsService.GetSettingOrDefault(Settings.SkippedResources) as string[],
-        dynamicRenderModeString: settingsService.GetSettingOrDefault(Settings.DynamicRenderModeString),
-        staticRenderModeString: settingsService.GetSettingOrDefault(Settings.StaticRenderModeString),
-        hybridRenderModeString: settingsService.GetSettingOrDefault(Settings.HybridRenderModeString),
-        bundleScriptRegex: new RegExp(settingsService.GetSettingOrDefault(Settings.BundleScriptRegex) as string),
-        unsupportedBrowserScript: settingsService.GetSettingOrDefault(Settings.UnsupportedBrowserScript)
-      };
-
-      const errors = new Array();
-      const siteMap = new Array();
-      const addEntrySiteMap = (loc) => siteMap.push({
-        loc: loc,
-        lastmod: (new Date().toLocaleDateString().replace(/\//g, '-')),
-        changefreq: 'weekly',
-        priority: 1
-      });
-
-      async function getPathsForLinksOnPage(page) {
-        await page.waitForSelector('a', { timeout: 5000 });
-
-        const linkPaths = await page.evaluate(() => {
-          const pathNames = Array<string>();
-          const linkElements = document.querySelectorAll('a');
-
-          // @ts-ignore
-          for (const element of linkElements) {
-            if (element.href.indexOf(document.location.origin) >= 0) {
-              pathNames.push(element.pathname);
-            }
-          }
-
-          return pathNames;
-        });
-
-        return linkPaths;
-      }
-
-      async function renderPage(page, route) {
-        const pageName = route === Strings.Empty ? 'index' : route;
-        const url = `${config.baseUrl}${route}`;
-
-        console.log(`| ========= Attempting to crawl: ${pageName} ========= |`);
-
-        await page.goto(url, { waitUntil: 'networkidle2' });
-
-        let content = await page.content();
-        content = content.replace(/\r?\n|\r/g, Strings.Empty).replace(/>\s+</g, '><');
-
-        if (content.indexOf(config.staticRenderModeString) >= 0) {
-          content = content.replace(config.bundleScriptRegex, Strings.Empty);
-        }
-
-        if (content.indexOf(config.dynamicRenderModeString) < 0) {
-          await fse.outputFile(`${config.outputPathRoot}/${pageName}.html`, content);
-          addEntrySiteMap(url);
-        } else {
-          const bundleScript = content.match(config.bundleScriptRegex)[0] || Strings.Empty;
-          const appRootDivString = '<div id="app-root">';
-          const closingHtml = `${appRootDivString}</div>${config.unsupportedBrowserScript}${bundleScript}</body></html>`;
-
-          if (content.indexOf(appRootDivString) >= 0) {
-            let unRenderedVersion = content.split(appRootDivString)[0];
-            unRenderedVersion = `${unRenderedVersion}${closingHtml}`;
-            await fse.outputFile(`${config.outputPathRoot}/${pageName}.html`, unRenderedVersion);
-            addEntrySiteMap(url);
-          }
-        }
-
-        return await getPathsForLinksOnPage(page);
-      }
-
-      function requestIsMediaOrBlockedResource(pageRequest, url) {
-        return config.blockedResourceTypes.indexOf(pageRequest.resourceType()) !== -1 ||
-          config.skippedResources.some(resource => url.indexOf(resource) !== -1);
-      }
-
-      async function excludeMediaAndIntegrations(page) {
-        await page.setRequestInterception(true);
-
-        page.on('request', (pageRequest) => {
-          const url = pageRequest._url.split('?')[0].split('#')[0];
-          if (requestIsMediaOrBlockedResource(pageRequest, url)) {
-            pageRequest.abort();
-          } else {
-            pageRequest.continue();
-          }
-        });
-      }
-
       const getXmlSiteMap = () => {
         const xmlStart = `<?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">`;
         const xmlEnd = '</urlset>';
 
         let xmlContent = Strings.Empty;
-        siteMap.forEach(url => {
+        this.siteMap.forEach(url => {
           xmlContent += `
         <url>
           <loc>${url.loc}</loc>
@@ -129,42 +68,117 @@ export class PreRenderOperation implements IOperation {
       };
 
       (async () => {
-        const browser = await puppeteer.launch();
+        const browser = await this.browser.launch();
         const page = await browser.newPage();
-        await excludeMediaAndIntegrations(page);
+        await this.excludeMediaAndIntegrations(page);
 
-        const pagesToCrawl = [Strings.Empty];
-        const crawledPages = ['/'];
-
-        while (pagesToCrawl.length >= 1) {
-          const pageToCrawl = pagesToCrawl.shift();
+        while (this.pagesToCrawl.length >= 1) {
+          const pageToCrawl = this.pagesToCrawl.shift();
 
           try {
-            const linksOnPage = await renderPage(page, pageToCrawl);
+            const linksOnPage = await this.renderPage(page, pageToCrawl);
 
             linksOnPage.forEach(linkHref => {
-              if (crawledPages.indexOf(linkHref) < 0 && pagesToCrawl.indexOf(linkHref) < 0) {
-                pagesToCrawl.push(linkHref);
+              if (this.crawledPages.indexOf(linkHref) < 0 && this.pagesToCrawl.indexOf(linkHref) < 0) {
+                this.pagesToCrawl.push(linkHref);
               }
             });
           } catch (error) {
             console.error(error);
-            errors.push({ page: pageToCrawl, error: error.toString() });
-          }
-
-          if (pageToCrawl) {
-            crawledPages.push(pageToCrawl);
+            this.errors.push({ page: pageToCrawl, error: error.toString() });
           }
         }
 
-        await fse.outputFile(`${config.outputPathRoot}/sitemap.json`, JSON.stringify({ urlset: siteMap }));
-        await fse.outputFile(`${config.outputPathRoot}/sitemap.xml`, getXmlSiteMap());
-        await fse.outputFile(`${config.outputPathRoot}/errors.json`, JSON.stringify(errors));
+        await this.fse.outputFile(`${this.config.outputPathRoot}/sitemap.json`, JSON.stringify({ urlset: this.siteMap }));
+        await this.fse.outputFile(`${this.config.outputPathRoot}/sitemap.xml`, getXmlSiteMap());
+        await this.fse.outputFile(`${this.config.outputPathRoot}/errors.json`, JSON.stringify(this.errors));
 
-        console.log(`Completed crawling ${crawledPages.length} pages with ${errors.length} errors.`);
+        console.log(`Completed crawling ${this.crawledPages.length} pages with ${this.errors.length} errors.`);
 
         await browser.close();
       })();
     }).Execute();
+  }
+
+  private addEntrySiteMap(loc): void {
+    this.siteMap.push({
+      loc: loc,
+      lastmod: (new Date().toLocaleDateString().replace(/\//g, '-')),
+      changefreq: 'weekly',
+      priority: 1
+    });
+  }
+
+  private async excludeMediaAndIntegrations(page) {
+    await page.setRequestInterception(true);
+
+    page.on('request', (pageRequest) => {
+      const url = pageRequest._url.split('?')[0].split('#')[0];
+      if (this.requestIsMediaOrBlockedResource(pageRequest, url)) {
+        pageRequest.abort();
+      } else {
+        pageRequest.continue();
+      }
+    });
+  }
+
+  private async getPathsForLinksOnPage(page) {
+    await page.waitForSelector('a', { timeout: 5000 });
+
+    const linkPaths = await page.evaluate(() => {
+      const pathNames = Array<string>();
+      const linkElements = document.querySelectorAll('a');
+
+      // @ts-ignore
+      for (const element of linkElements) {
+        if (element.href.indexOf(document.location.origin) >= 0) {
+          pathNames.push(element.pathname);
+        }
+      }
+
+      return pathNames;
+    });
+
+    return linkPaths;
+  }
+
+  private requestIsMediaOrBlockedResource(pageRequest, url) {
+    return this.config.blockedResourceTypes.indexOf(pageRequest.resourceType()) !== -1 ||
+      this.config.skippedResources.some(resource => url.indexOf(resource) !== -1);
+  }
+
+  private async renderPage(page, route) {
+    this.crawledPages.push(route);
+    const pageName = route === Strings.Empty ? 'index' : route;
+    const url = `${this.config.baseUrl}${route}`;
+
+    console.log(`| ========= Attempting to crawl: ${pageName} ========= |`);
+
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    let content = await page.content();
+    content = content.replace(/\r?\n|\r/g, Strings.Empty).replace(/>\s+</g, '><');
+
+    if (content.indexOf(this.config.staticRenderModeString) >= 0) {
+      content = content.replace(this.config.bundleScriptRegex, Strings.Empty);
+    }
+
+    if (content.indexOf(this.config.dynamicRenderModeString) < 0) {
+      await this.fse.outputFile(`${this.config.outputPathRoot}/${pageName}.html`, content);
+      this.addEntrySiteMap(url);
+    } else {
+      const bundleScript = content.match(this.config.bundleScriptRegex)[0] || Strings.Empty;
+      const appRootDivString = '<div id="app-root">';
+      const closingHtml = `${appRootDivString}</div>${this.config.unsupportedBrowserScript}${bundleScript}</body></html>`;
+
+      if (content.indexOf(appRootDivString) >= 0) {
+        let unRenderedVersion = content.split(appRootDivString)[0];
+        unRenderedVersion = `${unRenderedVersion}${closingHtml}`;
+        await this.fse.outputFile(`${this.config.outputPathRoot}/${pageName}.html`, unRenderedVersion);
+        this.addEntrySiteMap(url);
+      }
+    }
+
+    return await this.getPathsForLinksOnPage(page);
   }
 }
