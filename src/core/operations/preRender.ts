696132879a79c4a143c8e81d0f8848f975715184
Joey Bayes <joey.bayes@uscca.com>
2021-09-23 09:43:25 -0400

    Add additional flag to prerender launch options.

--- src/core/operations/preRender.ts
+++ src/core/operations/preRender.ts
@@ -97,7 +97,7 @@ export class PreRenderOperation implements IOperation {
         res.status(404).sendFile(path.join(process.cwd() + '/public/index.html'));
       });
 
-      const browser = await this.browser.launch({ args: ['--disable-gpu'] });
+      const browser = await this.browser.launch({ args: ['--disable-gpu', '--disable-dev-shm-usage'] });
       const page = await browser.newPage();
       await this.excludeMediaAndIntegrations(page);
 
