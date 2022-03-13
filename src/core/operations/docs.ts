0bf7336bc3c47daa868f2e49499dc93859478170
Joseph Bayes <joseph.w.bayes@outlook.com>
2022-03-12 23:16:37 -0500

    For docs operation, fallback to "start" command if an error occurs using "open" command.

--- src/core/operations/docs.ts
+++ src/core/operations/docs.ts
@@ -9,9 +9,24 @@ export class DocsOperation implements IOperation {
 
   public async Execute(args: string[]): Promise<Result> {
     return await new AsyncCommand(async () => {
+      const docsPageOrigin = 'https://fyord.dev/docs?search=';
       const query = encodeURIComponent(args.join(' '));
+      const docsPageUrl = `${docsPageOrigin}${query}`;
 
-      this.cp.exec(`open https://fyord.dev/docs?search=${query}`);
+      await this.cp.exec(`open ${docsPageUrl}`, async stderr => {
+        if (stderr) {
+          this.executeUsingStartCommand(docsPageUrl);
+        }
+      });
     }).Execute();
+  }
+
+  private async executeUsingStartCommand(docsPageUrl: string): Promise<void> {
+    await this.cp.exec(`start ${docsPageUrl}`, stderr => {
+      if (stderr) {
+        throw new Error(`Unable to open system browser via terminal using "open" or "start" command(s).
+You may still visit the docs page directly at: ${docsPageUrl}`);
+      }
+    });
   }
 }
