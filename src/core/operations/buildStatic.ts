00b789f3c9a4a9ab41772e1d1c6ad7bc669bd0ee
Joey Bayes <joey.bayes@uscca.com>
2022-04-18 10:23:03 -0400

    Add build static command to operation.

--- src/core/operations/buildStatic.ts
+++ src/core/operations/buildStatic.ts
@@ -1,10 +1,16 @@
 import { AsyncCommand, Result } from 'tsbase';
+import * as child_process from 'child_process';
+import { DIModule } from '../../diModule';
 import { IOperation } from './operation';
 
 export class BuildStaticOperation implements IOperation {
+  constructor(
+    private cp: typeof child_process = DIModule.ChildProcess
+  ) {}
+
   public async Execute(_args: string[]): Promise<Result> {
     return await new AsyncCommand(async () => {
-      console.log('got here');
+      this.cp.execSync('npm run build-static');
     }).Execute();
   }
 }
