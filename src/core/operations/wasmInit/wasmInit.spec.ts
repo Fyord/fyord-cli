5f3568972f29ed43ea1fc667c5cc41ba64d27f9d
Joey Bayes <joey.bayes@uscca.com>
2022-04-27 14:46:52 -0400

    Update wasmInit tests.

--- src/core/operations/wasmInit/wasmInit.spec.ts
+++ src/core/operations/wasmInit/wasmInit.spec.ts
@@ -1,30 +1,34 @@
 import { Strings } from 'tsbase';
 import { Mock, Times } from 'tsmockit';
+import { Directories } from '../../../enums/directories';
+import { Errors } from '../../../enums/module';
 import { IFileSystemExtraAdapter } from '../../../fileSystem/fileSystemExtraAdapter';
-import { UpdateTextInFile } from '../../utility/updateTextInFile';
 import { WasmInit } from './wasmInit';
 
 describe('WasmInit', () => {
   let classUnderTest: WasmInit;
   const mockFsExtra = new Mock<IFileSystemExtraAdapter>();
-  const mockChildProcess = new Mock<any>();
 
-  let updateTextInFileCalledCount = 0;
-  const fakeUpdateTextInFile: UpdateTextInFile = async (
-    _fileName: string,
-    _oldText: string,
-    _newText: string
-  ): Promise<void> => { updateTextInFileCalledCount++; };
+  let updateTextInFileCalledTimes: number;
+  let installDependencyCalledTimes: number;
+  let addWebpackOnBuildCalledTimes: number;
 
   beforeEach(() => {
-    updateTextInFileCalledCount = 0;
+    updateTextInFileCalledTimes = 0;
+    installDependencyCalledTimes = 0;
+    addWebpackOnBuildCalledTimes = 0;
+
     mockFsExtra.Setup(fse => fse.outputFile(Strings.Empty, Strings.Empty));
-    mockChildProcess.Setup(cp => cp.execSync(Strings.Empty));
+    mockFsExtra.Setup(fse => fse.pathExists(Strings.Empty), false);
+    mockFsExtra.Setup(fse => fse.pathExists(Directories.CargoLock), false);
+    mockFsExtra.Setup(fse => fse.pathExists(Directories.RootPackage), false);
 
     classUnderTest = new WasmInit(
       mockFsExtra.Object,
-      mockChildProcess.Object,
-      fakeUpdateTextInFile);
+      () => Promise.resolve().then(() => { updateTextInFileCalledTimes++; }),
+      () => Promise.resolve().then(() => { installDependencyCalledTimes++; }),
+      () => Promise.resolve().then(() => { addWebpackOnBuildCalledTimes++; })
+    );
   });
 
   it('should construct', () => {
@@ -32,23 +36,33 @@ describe('WasmInit', () => {
     expect(new WasmInit()).toBeDefined();
   });
 
-  it('should fail when executed in a directory WITHOUT a package.json', async () => {
-    mockFsExtra.Setup(fse => fse.pathExists(Strings.Empty), false);
+  it('should fail when executed in a directory WITHOUT a package.json or Cargo.lock', async () => {
+    const result = await classUnderTest.Execute();
+
+    expect(result.IsSuccess).toBeFalsy();
+    expect(result.ErrorMessages).toContain(Errors.NotInRoot);
+    mockFsExtra.Verify(fse => fse.outputFile(Strings.Empty, Strings.Empty), Times.Never);
+  });
+
+  it('should fail when executed in a directory WITHOUT a package.json AND Cargo.lock', async () => {
+    mockFsExtra.Setup(fse => fse.pathExists(Directories.CargoLock), true);
 
     const result = await classUnderTest.Execute();
 
     expect(result.IsSuccess).toBeFalsy();
+    expect(result.ErrorMessages).toContain(Errors.WasmInitAlreadyRan);
     mockFsExtra.Verify(fse => fse.outputFile(Strings.Empty, Strings.Empty), Times.Never);
-    expect(updateTextInFileCalledCount).toEqual(0);
   });
 
-  it('should succeed when executed in a directory WITH a package.json', async () => {
-    mockFsExtra.Setup(fse => fse.pathExists(Strings.Empty), true);
+  it('should succeed when executed in a directory WITH a package.json and NO Cargo.lock', async () => {
+    mockFsExtra.Setup(fse => fse.pathExists(Directories.RootPackage), true);
 
     const result = await classUnderTest.Execute();
 
     expect(result.IsSuccess).toBeTruthy();
+    expect(updateTextInFileCalledTimes).toEqual(4);
+    expect(installDependencyCalledTimes).toEqual(2);
+    expect(addWebpackOnBuildCalledTimes).toEqual(1);
     mockFsExtra.Verify(fse => fse.outputFile(Strings.Empty, Strings.Empty), 6);
-    expect(updateTextInFileCalledCount).toEqual(6);
   });
 });
