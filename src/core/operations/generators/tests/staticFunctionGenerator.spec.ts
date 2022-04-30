9b8d2c921a14da512afe79a127c4dee79543f979
Joey Bayes <joey.bayes@uscca.com>
2022-04-30 14:09:32 -0400

    Test StaticFunctionGenerator.

---
+++ src/core/operations/generators/tests/staticFunctionGenerator.spec.ts
@@ -0,0 +1,35 @@
+import { Mock } from 'tsmockit';
+import { IFileSystemExtraAdapter } from '../../../../fileSystem/fileSystemExtraAdapter';
+import { StaticFunctionGenerator } from '../staticFunctionGenerator';
+
+describe('StaticFunctionGenerator', () => {
+  const mockFseAdapter = new Mock<IFileSystemExtraAdapter>();
+  let classUnderTest: StaticFunctionGenerator;
+
+  beforeEach(() => {
+    classUnderTest = new StaticFunctionGenerator(mockFseAdapter.Object);
+  });
+
+  it('should construct', () => {
+    expect(classUnderTest).toBeDefined();
+  });
+
+  it('should return failing result when no args given', async () => {
+    const result = await classUnderTest.Generate([]);
+    expect(result.IsSuccess).toBeFalsy();
+  });
+
+  it('should return failing result when no extension arg given', async () => {
+    const result = await classUnderTest.Generate(['name']);
+    expect(result.IsSuccess).toBeFalsy();
+  });
+
+  it('should return successful result and output two files when name and extension args are given', async () => {
+    mockFseAdapter.Setup(f => f.outputFile('', ''));
+
+    const result = await classUnderTest.Generate(['name', 'json']);
+
+    mockFseAdapter.Verify(f => f.outputFile('', ''), 2);
+    expect(result.IsSuccess).toBeTruthy();
+  });
+});
