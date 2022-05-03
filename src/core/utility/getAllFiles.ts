e9101c8e07a874a1de7b31f21af908437ac04fd4
Joey Bayes <joey.bayes@uscca.com>
2022-05-03 17:44:24 -0400

    Remove unnecesarry assignment.

--- src/core/utility/getAllFiles.ts
+++ src/core/utility/getAllFiles.ts
@@ -7,8 +7,6 @@ export const getAllFiles = (
 ) => {
   const files = fs.readdirSync(dirPath);
 
-  arrayOfFiles = arrayOfFiles;
-
   files.forEach((file) => {
     if (fs.statSync(dirPath + '/' + file).isDirectory()) {
       arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles, fs);
