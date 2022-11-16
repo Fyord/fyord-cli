6b30d18d4025bac5fa04f1e41a67f40ecb3b3267
Joey Bayes <joey.bayes@uscca.com>
2022-11-16 10:34:44 -0500

    Update new operation to add electron dependencies.

--- src/core/operations/electronInit/electronInit.ts
+++ src/core/operations/electronInit/electronInit.ts
@@ -1,12 +1,25 @@
 import { AsyncCommand, Result } from 'tsbase';
+import { DIModule } from '../../../diModule';
+import { Commands, Directories } from '../../../enums/module';
+import { IFileSystemExtraAdapter } from '../../../fileSystem/fileSystemExtraAdapter';
+import { installDependencyIfNotInstalled } from '../../utility/module';
 import { IOperation } from '../operation';
 
 export class ElectronInitOperation implements IOperation {
-  constructor() { }
+  constructor(
+    private fse: IFileSystemExtraAdapter = DIModule.FileSystemExtraAdapter,
+    private installDependencyIfNotInstalledFunc = installDependencyIfNotInstalled
+  ) { }
 
   public Execute(): Promise<Result<null>> {
     return new AsyncCommand(async () => {
-      console.log('Electron init operation scaffolded!');
+      const inRootDir = await this.fse.pathExists(Directories.RootPackage);
+
+      if (inRootDir) {
+        await this.installDependencyIfNotInstalledFunc(Directories.WebpackShellPlugin, Commands.InstallWebpackShellPlugin);
+        await this.installDependencyIfNotInstalledFunc(Directories.WebpackShellPlugin, Commands.InstallElectron);
+        await this.installDependencyIfNotInstalledFunc(Directories.WebpackShellPlugin, Commands.InstallElectronPackager);
+      }
     }).Execute();
   }
 }
