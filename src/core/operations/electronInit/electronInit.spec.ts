ae63eee312ea71f09ead7829741fefd45ca250e5
Joey Bayes <joey.bayes@uscca.com>
2022-11-19 11:32:37 -0500

    Test electronInit operation branches.

--- src/core/operations/electronInit/electronInit.spec.ts
+++ src/core/operations/electronInit/electronInit.spec.ts
@@ -1,8 +1,12 @@
-import { Mock } from 'tsmockit';
+import { Any, Mock, Times } from 'tsmockit';
 import { DIModule } from '../../../diModule';
 import { IFileSystemExtraAdapter } from '../../../fileSystem/module';
 import { IOperation } from '../operation';
 import { ElectronInitOperation } from './electronInit';
+import { ElectronMain } from './templates/electronMain';
+import { ElectronPreload } from './templates/electronPreload';
+import { ElectronRenderer } from './templates/electronRenderer';
+import { Routes } from './templates/routes';
 
 describe('ElectronInitOperation', () => {
   const mockFse = new Mock<IFileSystemExtraAdapter>();
@@ -29,6 +33,9 @@ describe('ElectronInitOperation', () => {
   beforeEach(() => {
     dependencyDirectories = [];
     dependencyCommands = [];
+    replacementFilePaths = [];
+    replacementOldValues = [];
+    replacementNewValues = [];
 
     classUnderTest = new ElectronInitOperation(mockFse.Object, fakeInstallDependencyFunc, fakeUpdateTextInFileFunc, mockFs.Object);
   });
@@ -36,5 +43,36 @@ describe('ElectronInitOperation', () => {
   it('should construct', () => {
     expect(classUnderTest).toBeDefined();
     expect(new ElectronInitOperation()).toBeDefined();
+  });
+
+  it('should return a failing result on any error', async () => {
+    const result = await classUnderTest.Execute([]);
+    expect(result.IsSuccess).toBeFalsy();
+  });
+
+  it('should NOT make any changes when not in the root directory', async () => {
+    mockFse.SetupOnce(f => f.pathExists(Any<string>()), false);
+    const result = await classUnderTest.Execute([]);
+    expect(result.IsSuccess).toBeTruthy();
+  });
+
+  it('should make changes to support electron when in the root directory', async () => {
+    mockFse.SetupOnce(f => f.pathExists(Any<string>()), true);
+    mockFse.Setup(f => f.outputFile(Any<string>(), ElectronMain));
+    mockFse.Setup(f => f.outputFile(Any<string>(), ElectronPreload));
+    mockFse.Setup(f => f.outputFile(Any<string>(), ElectronRenderer));
+    mockFse.Setup(f => f.outputFile(Any<string>(), Routes));
+    mockFs.SetupOnce(f => f.rmSync(Any<string>()));
+
+    const result = await classUnderTest.Execute([]);
+
+    expect(replacementFilePaths.length).toEqual(16);
+    expect(replacementOldValues.length).toEqual(16);
+    expect(replacementNewValues.length).toEqual(16);
+    mockFse.Verify(f => f.outputFile(Any<string>(), ElectronMain), Times.Once);
+    mockFse.Verify(f => f.outputFile(Any<string>(), ElectronPreload), Times.Once);
+    mockFse.Verify(f => f.outputFile(Any<string>(), ElectronRenderer), Times.Once);
+    mockFse.Verify(f => f.outputFile(Any<string>(), Routes), Times.Once);
+    expect(result.IsSuccess).toBeTruthy();
   });
 });
